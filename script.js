9a6a051d0ac83b4a46bdb5060c1b8e397c172c6e
MooMooCow719 <loctran7402@gmail.com>
2024-09-02 17:47:44 -0700

    bugfixes

--- script.js
+++ script.js
@@ -6,25 +6,32 @@ function makeGrid(){
     let board = document.getElementById('main-board');
     board.innerHTML = '';
 
-    for(let i=0; i<horizLines; i++){
+
+    for(let i = 0; i < horizLines; i++){
         let horizLine = document.createElement('div');
         horizLine.className = "horiz-line";
+        horizLine.style.top = `${(i / horizLines) * 100}%`;
         board.appendChild(horizLine);
     }
 
-    for(let i=0; i<measures; i++){
+
+    for(let i = 0; i < measures; i++){
         let measure = document.createElement('div');
         measure.className = "measure";
-        for(let j=0; j<notesPerMeasure; j++){
+        measure.style.left = `${(i / measures) * 100}%`;
+        measure.style.width = `${100 / measures}%`; 
+        for(let j = 0; j < notesPerMeasure; j++){
             let noteDiv = document.createElement('div');
             noteDiv.className = "note-div";
-            noteDiv.style.left = `${(j/notesPerMeasure) * 100}%`;
+            noteDiv.style.left = `${(j / notesPerMeasure) * 100}%`;
             measure.appendChild(noteDiv);
         }
+        
         board.appendChild(measure);
     }
 }
 
 makeGrid();
+
 
 
