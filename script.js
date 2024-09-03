86b79cd177d69c235e530509949bd0327dba138a
MooMooCow719 <loctran7402@gmail.com>
2024-09-02 17:38:09 -0700

    grid bugfix

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function makeGrid(){
     let board = document.getElementById('main-board');
     board.innerHTML = '';
 
+    for(let i=0; i<horizLines; i++){
+        let horizLine = document.createElement('div');
+        horizLine.className = "horiz-line";
+        board.appendChild(horizLine);
+    }
+
     for(let i=0; i<measures; i++){
         let measure = document.createElement('div');
         measure.className = "measure";
-        for(let j=0; j<notesPerMeasure; i++){
+        for(let j=0; j<notesPerMeasure; j++){
             let noteDiv = document.createElement('div');
             noteDiv.className = "note-div";
             noteDiv.style.left = `${(j/notesPerMeasure) * 100}%`;
